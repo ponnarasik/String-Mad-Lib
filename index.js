b5e800b94564100c95a5a6c5687af1599cd74c6f
ponnarasi <>
2021-06-07 22:40:29 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let pluralNoun = "tables";
+let name = "Arasi" ;
+let verb = "swim";
+let adjective = "elegant";
+let color ="green" ;
+
+console.log("JavaScript provides a "+ color +" collection of tools — including " + adjective + " syntax and " + pluralNoun + " — that allows "+ name +" to "+ verb +" with strings.");
